07456b4d8a5a6f65ea13565008c178301cf0864f
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-02-05 20:26:29 +0530

    Create FindKClosetElement.cpp

---
+++ Day 02/FindKClosetElement.cpp
@@ -0,0 +1,30 @@
+
+
+
+                                                                 // Find K closest elements //
+
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int low=0,high=arr.size()-k;
+        while(low<high)
+        {
+            int mid=low+(high-low)/2;
+            if(x-arr[mid]>arr[mid+k]-x)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid;
+            }
+        }
+        vector<int>ans;
+        for(int i=low; i<low+k; i++)
+        {
+            ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+};
